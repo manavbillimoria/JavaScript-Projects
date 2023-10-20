ad34dbf1da19b806f5308a75e7f9ef8465281db0
manavbillimoria <manavb632004@gmail.com>
2023-10-20 19:54:13 +0530

    setTimeout issue fixed

--- Digital Clock/src/script.js
+++ Digital Clock/src/script.js
@@ -1,5 +1,8 @@
+
+var t1,t2;
 function get12hrTime()
 {
+      
       var date=new Date();
       var hh=date.getHours();
       var mm=date.getMinutes();
@@ -18,9 +21,40 @@ function get12hrTime()
       mm=(mm<10)? "0" + mm : mm;
       ss=(ss<10)? "0" + ss : ss;
 
-      let time = hh + "." + mm + "." + ss + " "+session;
+      let time = hh + ":" + mm + ":" + ss + " "+session;
       document.getElementById("time").innerHTML = time;
-      let t=setTimeout(function(){
+      if(t2)
+      {
+            clearTimeout(t2);
+      }
+      t1=setTimeout(function(){
             get12hrTime()
+      },1000);
+}
+function get24hrTime()
+{
+      
+      var date=new Date();
+      var hh=date.getHours();
+      var mm=date.getMinutes();
+      var ss=date.getSeconds();
+      var session="AM";
+      
+      if(hh>12)
+      {
+            session = "PM";
+      }
+      hh=(hh<10)? "0" + hh : hh;
+      mm=(mm<10)? "0" + mm : mm;
+      ss=(ss<10)? "0" + ss : ss;
+
+      let time = hh + ":" + mm + ":" + ss + " "+session;
+      document.getElementById("time").innerHTML = time;
+      if(t1)
+      {
+            clearTimeout(t1);
+      }
+      t2=setTimeout(function(){
+            get24hrTime()
       },1000);
 }
